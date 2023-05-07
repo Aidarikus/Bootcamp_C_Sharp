e34fbf9b2afa7c8c31a351e3c5c51e48541fb900
Aidar <adrizakirov@gmail.com>
2023-05-07 22:11:49 +0900

    Занятие 2

---
+++ Online/1/01/Program.cs
@@ -0,0 +1,26 @@
+﻿// MyList li = new();
+// Console.WriteLine(li.Print());
+
+// for (int i = 0; i < 10; i++) li.Created(Random.Shared.Next(100));
+
+// for (int i = 0; i < 10; i++) li.Created(i + 1);
+
+// Console.WriteLine(li.Read(5));
+// li.Update(0, 100);
+// Console.WriteLine(li.Print());
+// li.DeleteAt(5);
+// Console.WriteLine(li.Print());  // Работа с массивом
+
+// Node n1 = new Node(1);
+// n1.Next = new Node(2);
+// n1.Next.Next = new Node(3);
+// n1.Next.Next.Next = new Node(4);
+// n1.Next.Next.Next.Next = new Node(5);
+
+// Print(n1);
+// Console.WriteLine();
+
+// NodeList nl = new NodeList();
+// for (int i = 0; i < 10; i++) nl.Create(i + 1);
+
+// Console.WriteLine(nl.Print());  // Работа со списками
